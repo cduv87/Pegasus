216c35f5a07681d4793daf9ab3239026328be241
Philéas PERON <phileas.peron2022@campus-eni.fr>
2022-04-12 14:13:12 +0200

    création des bo

---
+++ src/main/java/model/bo/Categorie.java
@@ -0,0 +1,25 @@
+package model.bo;
+
+public class Categorie {
+	private int noCategorie;
+	private String libelle;
+	
+	public Categorie(int noCategorie, String libelle) {
+		super();
+		this.noCategorie = noCategorie;
+		this.libelle = libelle;
+	}
+	
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+}
