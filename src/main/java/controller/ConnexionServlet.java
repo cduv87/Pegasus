a86ce4ecec2d1ed3733ee62ae4c93d34a3fdbdd7
Antoine1989 <antoine-guillou@orange.fr>
2022-04-13 14:44:38 +0200

    ADD : Servlet Connexion et lien avec connexion.jsp #05

---
+++ src/main/java/controller/ConnexionServlet.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/connexion")
+public class ConnexionServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ConnexionServlet() {
+        super();
+}
+}
