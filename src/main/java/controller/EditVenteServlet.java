57658782836c32e99b5a1cf8f96769da87f7f695
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-04-15 15:22:30 +0200

    CHANGE : Mise en commantaire  de EditVenteServlet

--- src/main/java/controller/EditVenteServlet.java
+++ src/main/java/controller/EditVenteServlet.java
@@ -25,32 +25,32 @@ public class EditVenteServlet extends HttpServlet {
 
 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	try {			
-		String article = request.getParameter("article");
-		String description = request.getParameter("description");
-		Categorie categorie = request.getParameter("categorie");
-		
-		int miseAprix=request.getParameter("prix");
-		/*List<String>= StringToList();
-		
-		*/
-		String date = request.getParameter("D�but de l'ench�re");
-		String date2 =request.getParameter("Fin de l'ench�re");
-		
-		LocalDate dateDebutEncheres=LocalDate.parse(date);
-		LocalDate dateFinEncheres=LocalDate.parse(date2);
-		
-		// il faut cr�er un constructeur utilisateur (rue,codePostal, ville)
-		Utilisateur utilisateur = new Utilisateur (utilisateur.getRue(),utilisateur.getCodePostal(),utilisateur.getVille()) ;
-		
-		ArticleVendu articleVendu= new ArticleVendu (article,description,categorie,miseAprix,dateDebutEncheres,dateFinEncheres,utilisateur);
-		
-	
-		//Appel de la BLL
-		
-		this.articleManager.add(articleVendu);
-		
-		//Insertion
-	
+//		String article = request.getParameter("article");
+//		String description = request.getParameter("description");
+//		Categorie categorie = request.getParameter("categorie");
+//		
+//		int miseAprix=request.getParameter("prix");
+//		/*List<String>= StringToList();
+//		
+//		*/
+//		String date = request.getParameter("D�but de l'ench�re");
+//		String date2 =request.getParameter("Fin de l'ench�re");
+//		
+//		LocalDate dateDebutEncheres=LocalDate.parse(date);
+//		LocalDate dateFinEncheres=LocalDate.parse(date2);
+//		
+//		// il faut cr�er un constructeur utilisateur (rue,codePostal, ville)
+//		Utilisateur utilisateur = new Utilisateur (utilisateur.getRue(),utilisateur.getCodePostal(),utilisateur.getVille()) ;
+//		
+//		ArticleVendu articleVendu= new ArticleVendu (article,description,categorie,miseAprix,dateDebutEncheres,dateFinEncheres,utilisateur);
+//		
+//	
+//		//Appel de la BLL
+//		
+//		this.articleManager.add(articleVendu);
+//		
+//		//Insertion
+//	
 	}catch(Exception e){
 		
 	e.printStackTrace();
