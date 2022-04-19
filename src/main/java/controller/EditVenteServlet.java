716c7bbe62942690849155b51ddc62cf3eded8ab
Antoine1989 <antoine-guillou@orange.fr>
2022-04-19 09:25:56 +0200

    ADD: #02 affichage réponse getRequestDispatcher entre jsp editVente et EditVenteServlet

--- src/main/java/controller/EditVenteServlet.java
+++ src/main/java/controller/EditVenteServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.bll.ArticleManager;
+import model.bll.BusinessException;
 import model.bo.ArticleVendu;
 import model.bo.Categorie;
 import model.bo.Utilisateur;
@@ -17,46 +18,53 @@ import model.bo.Utilisateur;
 @WebServlet("/editVente")
 public class EditVenteServlet extends HttpServlet {
 	private ArticleManager articleManager = new ArticleManager();
-	
-	 public EditVenteServlet() {}
 
-	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	 	request.getRequestDispatcher("/WEB-INF/editVente.jsp").forward(request, response);
-	}
- 
+	public EditVenteServlet() {}
 
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	try {			
-		String article = request.getParameter("article");
-		String description = request.getParameter("description");
-		//En attente DAO ;   En attente  de l'énumération
-		//Categorie categorie = request.getParameter("categorie");
-		
-		int miseAprix=request.getParameter("prix");
-		/*List<String>= StringToList();
-		
-		*/
-		String date = request.getParameter("Début de l'enchère");
-		String date2 =request.getParameter("Fin de l'enchère");
-		
-		LocalDate dateDebutEncheres=LocalDate.parse(date);
-		LocalDate dateFinEncheres=LocalDate.parse(date2);
-		
-		// il faut créer un constructeur utilisateur (rue,codePostal, ville)
-	//	Utilisateur utilisateur = new Utilisateur (utilisateur.getRue(),utilisateur.getCodePostal(),utilisateur.getVille()) ;
-		
-		ArticleVendu articleVendu= new ArticleVendu (article,description,categorie,miseAprix,dateDebutEncheres,dateFinEncheres,utilisateur);
-		
-	
-		//Appel de la BLL
-		
-		this.articleManager.add(articleVendu);
-		
-		//Insertion
-	
-	}catch(Exception e){
-		
-	e.printStackTrace();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/editVente.jsp").forward(request, response);
 	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {			
+			String article = request.getParameter("article");
+			String description = request.getParameter("description");
+			//En attente DAO ;   En attente  de l'énumération
+			//Categorie categorie = request.getParameter("categorie");
+
+			int miseAprix=request.getParameter("prix");
+			/*List<String>= StringToList();
+
+			 */
+			String date = request.getParameter("Début de l'enchère");
+			String date2 =request.getParameter("Fin de l'enchère");
+
+			LocalDate dateDebutEncheres=LocalDate.parse(date);
+			LocalDate dateFinEncheres=LocalDate.parse(date2);
+
+			// il faut créer un constructeur utilisateur (rue,codePostal, ville)
+			//	Utilisateur utilisateur = new Utilisateur (utilisateur.getRue(),utilisateur.getCodePostal(),utilisateur.getVille()) ;
+
+			ArticleVendu articleVendu= new ArticleVendu (article,description,categorie,miseAprix,dateDebutEncheres,dateFinEncheres,utilisateur);
+
+
+			//Appel de la BLL
+
+			this.articleManager.add(articleVendu);
+
+			//Insertion
+
+		}
+		catch (BusinessException e){ 
+			request.setAttribute("messageErreur", e.getMessage());
+		}
+
+
+		// je délègue l'affichage de la réponse à la JSP
+		request.getRequestDispatcher("/WEB-INF/editVente.jsp").forward(request, response);
+
 	}
+
+
 }
