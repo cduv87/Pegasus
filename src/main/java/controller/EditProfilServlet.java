b84501a435da407aa632ff3d4d27c5ab33f0c02e
Clément Duval <103181914+cduv87@users.noreply.github.com>
2022-04-19 17:01:20 +0200

    ADD : servlet et jsp editprofile fonctionelle, minor bugs to correct

--- src/main/java/controller/EditProfilServlet.java
+++ src/main/java/controller/EditProfilServlet.java
@@ -6,6 +6,7 @@ import model.bo.Utilisateur;
 import model.bll.BusinessException;
 import model.bll.UtilisateurManager;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,21 +18,25 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/editprofil")
 public class EditProfilServlet extends HttpServlet {
-	
+
 	private UtilisateurManager userManager = new UtilisateurManager();
 	private Utilisateur utilisateur = new Utilisateur();
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("METHODE GET");
 		request.getRequestDispatcher("/WEB-INF/editProfil.jsp").forward(request, response);
 	}
 
-	protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("METHODE POST");
-		
-	
+
+		Utilisateur connecte = new Utilisateur();
+		HttpSession session = request.getSession();
+		;
 		String messageErreur = null;
 		final int creditsUser = 100;
 		String pseudo = null;
@@ -43,11 +48,11 @@ public class EditProfilServlet extends HttpServlet {
 		String codePostal = null;
 		String ville = null;
 		String motDePasse1 = null;
-		String motDePasse2 = null;	
+		String motDePasse2 = null;
 		String motDePasseActu = null;
-		
-		System.out.println(request.getParameter("pseudo"));
-		
+
+		System.out.println("pseudonyme : " + request.getParameter("pseudo"));
+
 		pseudo = request.getParameter("pseudo");
 		nom = request.getParameter("nom");
 		prenom = request.getParameter("prenom");
@@ -60,11 +65,12 @@ public class EditProfilServlet extends HttpServlet {
 		motDePasse2 = request.getParameter("mdp2");
 		motDePasseActu = request.getParameter("mdpactu");
 		
-		if ( ! motDePasse1.equals(motDePasse2)) {
+		System.out.println("mdp1 : " + motDePasse1 + "mdp2 : " + motDePasse2);
+		if (! motDePasse1.equals(motDePasse2)) {
 			request.setAttribute("erreur", "Vous avez inséré des mots de passe différents");
 			this.doGet(request, response);
 		}
-		
+
 		utilisateur.setPseudo(pseudo);
 		utilisateur.setNom(nom);
 		utilisateur.setPrenom(prenom);
@@ -74,26 +80,41 @@ public class EditProfilServlet extends HttpServlet {
 		utilisateur.setCodePostal(codePostal);
 		utilisateur.setVille(ville);
 		utilisateur.setMotDePasse(motDePasse1);
-		utilisateur.setCredit(creditsUser);
-			
-		
-		try {
-			userManager.ajouterUtilisateur(utilisateur);
-			HttpSession session = request.getSession();
-			session.setAttribute("utilisateurConnecte", utilisateur);
-			response.sendRedirect("./");
-		} catch (BusinessException | SQLException e) {
-			request.setAttribute("erreur", e.getMessage());
-			e.printStackTrace();
-			this.doGet(request, response);
+
+
+		if (session.getAttribute("utilisateurConnecte") != null) {
+			connecte = (Utilisateur) session.getAttribute("utilisateurConnecte");
+			System.out.println(connecte);
+			String mdpUser = connecte.getMotDePasse();
+			System.out.println("mdp session actuelle" +mdpUser);
+			System.out.println("mdp entré" +motDePasseActu);
+			if (! mdpUser.equals(motDePasseActu)) {
+				System.out.println("erreur ancien mot de passe mauvais");
+				request.setAttribute("erreur", "Votre ancien mot de passe n'est pas le bon");
+				this.doGet(request, response);
+			}
+			utilisateur.setNoUtilisateur(connecte.getNoUtilisateur());
+			try {
+				userManager.modifierUtilisateur(utilisateur);
+				session.setAttribute("utilisateurConnecte", utilisateur);
+				response.sendRedirect("./");
+			} catch (SQLException e) {
+				request.setAttribute("erreur", e.getMessage());
+				e.printStackTrace();
+				this.doGet(request, response);
+			}
+		} else {
+			try {
+				utilisateur.setCredit(creditsUser);
+				userManager.ajouterUtilisateur(utilisateur);
+				session.setAttribute("utilisateurConnecte", utilisateur);
+				response.sendRedirect("./");
+			} catch (BusinessException | SQLException e) {
+				request.setAttribute("erreur", e.getMessage());
+				e.printStackTrace();
+				this.doGet(request, response);
+			}
 		}
 	}
-	
-	
-	
-	
-	
 
-} //final
-
-
+} // final
